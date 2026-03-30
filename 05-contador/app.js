92218cfd34aa5694fd3d4262c95f71453480f04e
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2026-03-30 14:24:50 -0300

    Funções completas, iniciando a manipulação de tabelas

--- 05-contador/app.js
+++ 05-contador/app.js
@@ -1,5 +1,12 @@
 'use script'
+// function criarListaNumeros(quantidade){
+//     let listaNumeros = []
 
+//     for(let i = 1; i<= quantidade; i++){
+//         listaNumeros.push(i*2)
+//     }
+//     return listaNumeros
+// }
 function criarListaNumeros(quantidade){
     let listaNumeros = []
 
@@ -8,13 +15,13 @@ function criarListaNumeros(quantidade){
     }
     return listaNumeros
 }
-console.log(criarListaPares(13))
+console.log(criarListaNumeros(13))
 
 function criarListaPares(quantidade){
     let listaPar = []
     let quantidadeDois = Number(quantidade)*2
-    cont = 0
-    while(cont < quantidadeDois){
+    cont = 1
+    while(cont <= quantidadeDois){
         
         if(cont % 2 == 0){
             
@@ -26,3 +33,56 @@ function criarListaPares(quantidade){
     
     return listaPar
 }
+console.log(criarListaPares(13))
+
+function criarListaImpares(quantidade){
+    let listaImpar = []
+    let quantidadeDois = Number(quantidade)*2
+    cont = 0
+    while(cont <= quantidadeDois){
+        
+        if(cont % 2 != 0){
+            
+            listaImpar.push(cont)
+        }
+
+        cont++
+    }
+    
+    return listaImpar
+}
+console.log(criarListaImpares(13))
+
+function criarListaMultDe5(quantidade){
+    let listaMultDe5 = []
+    let cont = 1
+    let resultado
+    
+    while(cont <= quantidade){
+
+        resultado = cont*5
+        listaMultDe5.push(resultado)
+        cont++
+
+    }
+    
+    return listaMultDe5
+}
+console.log(criarListaMultDe5(13))
+
+function criarListaPotenc2(quantidade){
+    let listaPotenc2 = []
+    let cont = 0
+    let resultado
+    let potencia = 2
+
+    while(cont < quantidade){
+
+        resultado = potencia ** cont
+        listaPotenc2.push(resultado)
+        cont++
+    }
+
+    return listaPotenc2
+}
+console.log(criarListaPotenc2(13))
